91a88cda43d6f324a09eaaa3875a16a8822229c4
Hungai Amuhinda <hungaiinc@gmail.com>
2017-10-13 12:22:11 +0300

    Empty Sources

---
+++ src/main/scala/com/lightbend/faq/streams/sources/EmptySources.scala
@@ -0,0 +1,35 @@
+package com.lightbend.faq.streams.sources
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+/**
+  * Created by hungai on 13/10/2017.
+  */
+object EmptySources {
+
+  /**
+    * Empty Sources
+    */
+
+  implicit val system = ActorSystem("jump")
+  implicit val mat = ActorMaterializer()
+
+  import system.dispatcher
+
+  /**
+    * Source.empty -> Creates an empty Source of the specified type
+    *       -> Always completes the stream
+    *
+    * Useful to test how your system responds when there is no data.
+    *
+    */
+
+  val source: Source[String,NotUsed] = Source.empty[String]
+
+  source.runWith(Sink.foreach(println))
+    .onComplete(_ => system.terminate())
+
+}
